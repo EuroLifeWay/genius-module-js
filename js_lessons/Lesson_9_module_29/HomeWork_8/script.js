74990ed5bc67ce7fe02051d2dc911068594b186a
EuroLifeWay <eurolifeway@gmail.com>
2026-03-13 16:03:07 +0200

    feat: add alternative solution to HomeWork_8 Problem 1

--- js_lessons/Lesson_9_module_29/HomeWork_8/script.js
+++ js_lessons/Lesson_9_module_29/HomeWork_8/script.js
@@ -8,8 +8,28 @@
 // ------------- Problem 1/ Method 1 ------------- //
 // debugger;
 
-// const btn = document.querySelector('#btn');
-// const text = document.querySelector('#text');
+// // const btn = document.querySelector('#btn');
+// // const text = document.querySelector('#text');
+
+// btn.onclick = () => text.classList.toggle('is-hidden');
+
+//#region
+// ------------- Problem 1/ Method 2 ------------- //
+// debugger;
+
+// btn.addEventListener('click', () => text.classList.toggle('is-hidden'));
+
+//#region
+// ------------- Problem 1/ Method 3 ------------- //
+
+// btn.addEventListener(
+//   'click',
+//   () => (text.style.display = text.style.display === 'none' ? 'block' : 'none'),
+// );
+
+//#region
+// ------------- Problem 1/ Method 4 ------------- //
+// debugger;
 
 // const handleClick = () => {
 //   if (text.style.display === 'none') {
@@ -20,20 +40,6 @@
 // };
 
 // btn.addEventListener('click', handleClick);
-
-//#region
-// ------------- Problem 1/ Method 2 ------------- //
-
-// btn.addEventListener(
-//   'click',
-//   () => (text.style.display = text.style.display === 'none' ? 'block' : 'none'),
-// );
-
-//#region
-// ------------- Problem 1/ Method 3 ------------- //
-// debugger;
-
-// btn.addEventListener('click', () => text.classList.toggle('is-hidden'));
 
 //#region ------------------------------------------------------ //
 // ------------------------- Problem 2 ------------------------- //
@@ -61,41 +67,41 @@
 // ------------- Problem 3/ Method 1, 2, 3 ------------- //
 // debugger;
 
-const level = document.querySelector('#level');
-const btnCancel = document.querySelector('#btnCancel');
+// const level = document.querySelector('#level');
+// const btnCancel = document.querySelector('#btnCancel');
 
-// ------ Method 3.1 ------ //
-// children returns HTMLCollection - has no method .forEach()
-// Converting HTMLCollection to an Array
-const handleClick = event => {
-  [...event.target.children].forEach(child =>
-    child.classList.toggle('is-hidden'),
-  );
-};
-
-// ------ Method 3.2 ------ //
-// Select all children of the current element
+// // ------ Method 3.1 ------ //
+// // children returns HTMLCollection - has no method .forEach()
+// // Converting HTMLCollection to an Array
 // const handleClick = event => {
-//   event.target.querySelectorAll(':scope > *').forEach(child => {
-//     child.classList.toggle('is-hidden');
-//   });
+//   [...event.target.children].forEach(child =>
+//     child.classList.toggle('is-hidden'),
+//   );
 // };
 
-// ------ Method 3.3 ------ //
-// for...of can work with HTMLCollection directly
-// const handleClick = event => {
-//   for (const child of event.target.children) {
-//     child.classList.toggle('is-hidden');
-//   }
-// };
+// // ------ Method 3.2 ------ //
+// // Select all children of the current element
+// // const handleClick = event => {
+// //   event.target.querySelectorAll(':scope > *').forEach(child => {
+// //     child.classList.toggle('is-hidden');
+// //   });
+// // };
 
-level.addEventListener('click', handleClick);
+// // ------ Method 3.3 ------ //
+// // for...of can work with HTMLCollection directly
+// // const handleClick = event => {
+// //   for (const child of event.target.children) {
+// //     child.classList.toggle('is-hidden');
+// //   }
+// // };
 
-btnCancel.addEventListener('click', () =>
-  document
-    .querySelectorAll('.is-hidden')
-    .forEach(element => element.classList.remove('is-hidden')),
-);
+// level.addEventListener('click', handleClick);
+
+// btnCancel.addEventListener('click', () =>
+//   document
+//     .querySelectorAll('.is-hidden')
+//     .forEach(element => element.classList.remove('is-hidden')),
+// );
 
 // ------------------------------------ //
 // ------------------------------------ //
